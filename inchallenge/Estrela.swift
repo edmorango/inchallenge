3eb944eb4ba252d1c9dab29519439c19da4adad5
José Eduardo Vieira Morango <jevmor@gmail.comaa>
2016-07-20 14:22:05 -0400

    Criação do componente estrela,  ajustas do auto layout.

--- inchallenge/Estrela.swift
+++ inchallenge/Estrela.swift
@@ -17,156 +17,110 @@ class Estrela: UIView{
     @IBInspectable var number : Int = 1
     @IBInspectable var sidesNumber : Int = 5
     
+    
+    
     override func layoutSubviews() {
         
         super.layoutSubviews()
 
         
-        let spacingX = self.frame.width * 0.05
-        let minY = self.frame.height * 0.5
+        // Define o espaçamento em X entre os layers para 5% da tela
+        let spacingX = self.frame.width * 0.04
+       
+        //Define o espaçamento mínimo entre os shapes
+        let baseSpacingX = self.frame.width * 0.01
         
+        // Define a coordenada Y onde os layers serão desenhados
+        let baseY = self.frame.height * 0.5
         
-//        for i in 0...number{
-  
+        // Define altura e largura para 15% do tamanho dos respectivos pra cada layer
+        let width = self.frame.width * 0.1
+        let height = self.frame.height * 0.1
+        
+        for i in 1...number{
+        
         let shapeLayer = CAShapeLayer()
             
-            shapeLayer.lineWidth = 8
+            shapeLayer.lineWidth = 3
             shapeLayer.strokeColor = UIColor.blackColor().CGColor
             shapeLayer.fillColor = UIColor.whiteColor().CGColor
-        //            shapeLayer.path = drawBezier(self.frame.midX   , y: self.frame.midY, radius: 10, pointyness: 3).CGPath
-            shapeLayer.path = starPathInRect(CGRect(x: self.frame.midX, y: self.frame.midY, width: self.frame.midX, height: self.frame.midY)).CGPath
-        
+           
+            let rect  = CGRect(x: baseSpacingX + spacingX * CGFloat(i-1) + width * CGFloat(i-1) , y: baseY, width: width, height: height)
             
+            
+            shapeLayer.path = starBezierPath(rect)
+
+        
             layer.addSublayer(shapeLayer)
             
         
-//        }
-        
-        
-        
-        
+        }
     }
-
-    func pointFrom(angle: CGFloat, radius: CGFloat, offset: CGPoint) -> CGPoint {
+    
+    
+    
+    // Obtém as coordenadas pegando o ângulo e o raio
+    func pointFrom(angle angle: CGFloat, radius: CGFloat, offset: CGPoint) -> CGPoint {
         return CGPointMake(radius * cos(angle) + offset.x, radius * sin(angle) + offset.y)
     }
-
-    func starPathInRect(rect: CGRect) -> UIBezierPath {
+    
+    
+    func starBezierPath(rect : CGRect) -> CGPath{
+    
+        
         let path = UIBezierPath()
         
-        let starExtrusion:CGFloat = 25
         
-        let center = CGPointMake(rect.width / 1.0, rect.height / 1.0)
+        // Obtém o ponto central do rect
+        let center = CGPointMake(rect.midX, rect.midY)
+
         
-        let pointsOnStar = 5
-        var angle:CGFloat = -CGFloat(M_PI / 2.0)
-        let angleIncrement = CGFloat(M_PI * 2.0 / Double(pointsOnStar))
-        let radius = rect.width / 2.0
-        
-        var firstPoint = true
+        // Define a ângulo do shape, influencia na inclinação
+        var angle  = CGFloat(M_PI / 2.0)
         
         
+        // Divide o 360 graus em partes iguais
+        let angleIncrement = CGFloat(M_PI * 2.0 / Double(sidesNumber))
+  
         
-        for _ in 1...pointsOnStar {
+        // define como raio 30% da largura do rect, influencia na altura e largura
+        let radius = (rect.width * 0.35 + rect.height * 0.35 ) / 2
+        
+        
+        
+        // Calcula um dos pontos superiores e posiciona o path lá.
+        let point = pointFrom(angle: angle, radius: radius, offset: center)
+        path.moveToPoint(point)
+        
+        
+        
+        // Define os pontos e liga as linhas
+        for _ in 1...sidesNumber{
             
-            let point = pointFrom(angle, radius: radius, offset: center)
-            let nextPoint = pointFrom(angle + angleIncrement, radius: radius, offset: center)
-            let midPoint = pointFrom(angle + angleIncrement / 2.0, radius: starExtrusion, offset: center)
+            // Calcula um ponto mediano
+            let midPoint = pointFrom(angle: angle+angleIncrement / 2.0, radius: radius * 2, offset: center)
             
-            if firstPoint {
-                firstPoint = false
-                path.moveToPoint(point)
-            }
+            // Alterar o ângulo
+            angle += angleIncrement
             
+            // Calcula um ponto superior
+            let nextPoint = pointFrom(angle: angle, radius: radius, offset: center)
+        
+            // Desenha as linhas
             path.addLineToPoint(midPoint)
             path.addLineToPoint(nextPoint)
             
-            angle += angleIncrement
+            
+            
+            
+            
         }
         
         path.closePath()
-        
-        
-        return path
-    }
-
-    
-    
-    func generatePoints (x: CGFloat, y: CGFloat, radius: CGFloat, adj : CGFloat = 0) -> [CGPoint]{
-    
-        
-        var sides = sidesNumber
-        var points = [CGPoint]()
-        
-        let baseX  = x
-        let baseY  = y
-        let rad = radius
-
-        
-        // define o ângulo baseado na quantidade de lados do polígono
-        let angle = DrawUtils.degreeToRadian(360 / CGFloat(sides))
-        
-        
-        // calcula coordenadas de retas baseadas no ângulo
-        while points.count <= sides{
-       
-            let px = baseX - rad * cos(angle * CGFloat(sides) + DrawUtils.degreeToRadian(adj))
-            let py = baseY - rad * sin(angle * CGFloat(sides) + DrawUtils.degreeToRadian(adj))
-        
-            points.append(CGPoint(x: px, y: py))
-            
-            sides -= 1
-            
-            
-        }
-        
-        return points
+        return path.CGPath
     
     }
     
-    
-    func generatePath(x: CGFloat, y: CGFloat, radius : CGFloat, pointyness: CGFloat ) -> CGPathRef{
-    
-        // Calcula o ajuste
-        let adj = 360 / sidesNumber / 2
-        
-        let path = CGPathCreateMutable()
-        let points = generatePoints(x, y: y, radius: radius)
-       
-        let firstPoint = points.first!
-        
-        let pointsMin = generatePoints(x, y: y, radius: radius * pointyness, adj: CGFloat(adj))
-        
-        var i = 0
-        
-        CGPathMoveToPoint(path, nil, firstPoint.x, firstPoint.y)
-        
-        for p in points{
-        
-            CGPathAddLineToPoint(path, nil, pointsMin[i].x, pointsMin[i].y)
-            CGPathAddLineToPoint(path, nil, p.x, p.y)
-            
-            i += 1
-            
-        }
-        CGPathCloseSubpath(path)
-        
-        return path
-        
-        
-    }
-    
-    func drawBezier(x: CGFloat, y: CGFloat, radius : CGFloat, pointyness: CGFloat ) -> UIBezierPath{
-    
-    
-        let path = generatePath(x, y: y, radius: radius, pointyness: pointyness)
-        
-        let bezier = UIBezierPath(CGPath: path)
-        
-        return bezier
-        
-    
-    }
     
     
     
