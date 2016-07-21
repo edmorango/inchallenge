d8fbff4390324de6f9d001bb4ca62000851388f9
José Eduardo Vieira Morango <jevmor@gmail.comaa>
2016-07-20 20:11:52 -0400

    Finalizado o componente compartilhar.

--- inchallenge/Compartilhar.swift
+++ inchallenge/Compartilhar.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 
-
+@IBDesignable
 class Compartilhar: UIView{
 
     
@@ -19,104 +19,141 @@ class Compartilhar: UIView{
         
         super.layoutSubviews()
         
-        let shape = CAShapeLayer()
+        // Retângulo pai
+        let shapeCont = CAShapeLayer()
         
-        shape.lineWidth = 2
-        shape.fillColor = UIColor.whiteColor().CGColor
-        shape.strokeColor = UIColor.blackColor().CGColor
+        shapeCont.lineWidth = 2
+        shapeCont.fillColor = UIColor.whiteColor().CGColor
+        shapeCont.strokeColor = UIColor.blackColor().CGColor
         
-        let path = getCompartilhar(self.frame, scale: 0.7)
+        let pathCont = getCompartilhar(self.frame, scale: 0.7)
         
+        shapeCont.path = pathCont.CGPath
         
+        // Container da seta
+        let shapeSetaCont = CAShapeLayer()
         
-        shape.path = path.CGPath
-        //        path.stroke()
+        shapeSetaCont.fillColor = UIColor.whiteColor().CGColor
+        shapeSetaCont.lineWidth = 2
+        shapeSetaCont.strokeColor = UIColor.whiteColor().CGColor
         
-        layer.addSublayer(shape)
+        let pathSetaCont = getSetaContainer(self.frame, scale: 0.7)
+        
+        shapeSetaCont.path = pathSetaCont.CGPath
+      
+        // Seta
+        let shapeSeta = CAShapeLayer()
+
+
+        shapeSeta.lineWidth = 2
+        shapeSeta.fillColor = UIColor.blackColor().CGColor
+        shapeSeta.strokeColor = UIColor.blackColor().CGColor
+
+        let pathSeta = getSeta(self.frame, scale: 0.7)
+
+        shapeSeta.path = pathSeta.CGPath
+        
+
+        
+        // Atribui a seta ao container e o container ao pai.
+        shapeSetaCont.addSublayer(shapeSeta)
+        shapeCont.addSublayer(shapeSetaCont)
+        layer.addSublayer(shapeCont)
+        
         
         
     }
     
     
+    
+    
+    
+    
     func getCompartilhar(originalRect: CGRect, scale: Double) -> UIBezierPath {
         
         // Escala a largura do rect
-        let scaledWidth = (originalRect.size.width * CGFloat(scale))
+        let scaledWidth = (originalRect.size.width * CGFloat(scale) )
         
         //  Obtém a diferença da largura original para a escalada e divide por 2
         let scaledXValue = ((originalRect.size.width) - scaledWidth) / 2
         
-        // Escala a altura do rect
-        let scaledHeight = (originalRect.size.height * CGFloat(scale))
+        // Escala a altura do rect e a reduz em 30%
+        let scaledHeight = (originalRect.size.height * CGFloat(scale) *  0.7)
         
         //  Obtém a diferença da altura original para a escalada e divide por 2
         let scaledYValue = ((originalRect.size.height) - scaledHeight) / 2
         
-        // Cria o caminho
-        let path = UIBezierPath()
         
         // Cria um novo rect
         let scaledRect = CGRect(x: scaledXValue, y: scaledYValue, width: scaledWidth, height: scaledHeight)
+      
         
-        // Move o path para o centro em x e o limite de y(a base do coração)
-        path.moveToPoint(CGPointMake(scaledRect.midX, scaledRect.maxY))
-        
-        
-        
-        // Define um ponto na origem de x próximo a base de y ( sistema de coordenadas está |- ao invés de |_ )
-        let eCurvePoint = CGPointMake(scaledRect.origin.x, scaledRect.origin.y + scaledRect.size.height * 0.25)
-        // Define x próximo ao centro do rect e y próximo ao fim
-        let eControlPointOne = CGPointMake( scaledRect.origin.x + scaledRect.width * 0.5 , scaledRect.origin.y + scaledRect.size.height * 0.75)
-        // Define x na origem e y próximo ao centro do rect
-        let eControlPointTwo = CGPointMake( scaledRect.origin.x, scaledRect.origin.y + scaledRect.height * 0.5)
-        
-        path.addCurveToPoint(eCurvePoint,
-                             controlPoint1: eControlPointOne ,
-                             controlPoint2: eControlPointTwo )
-        
-        
-        
-        // Define o raio como 25% do tamanho do rect
-        let radius = scaledRect.size.width * 0.25
-        
-        
-        // Posiciona X e Y a 25% da largura e altura do rect para gerar um semi círculo em que o diamêtro cruze do começo ao meio do rect
-        path.addArcWithCenter(CGPointMake( scaledRect.origin.x + radius ,scaledRect.origin.y + scaledRect.size.height * 0.25),
-                              radius: radius,
-                              startAngle: CGFloat(M_PI),
-                              endAngle: 0,
-                              clockwise: true)
-        
-        
-        
-        // Posiciona X a 75% para o diamêtro do círculo cobrir o restante e mantém Y em 25%
-        path.addArcWithCenter(CGPointMake( scaledRect.origin.x + radius * 3,scaledRect.origin.y + scaledRect.size.height * 0.25),
-                              radius: (scaledRect.size.width/4),
-                              startAngle: CGFloat(M_PI),
-                              endAngle: 0,
-                              clockwise: true)
-        
-        
-        
-        // Basciamente um scale a -1 em todos pontos da outra curva
-        let dCurvePoint = CGPointMake(scaledRect.midX , scaledRect.origin.y + scaledRect.size.height)
-        let dControlPointOne =  CGPointMake( scaledRect.origin.x + scaledRect.size.width ,  scaledRect.origin.y + scaledRect.height * 0.5 )
-        let dControlPointTwo = CGPointMake(scaledRect.origin.x + scaledRect.width * 0.5, scaledRect.origin.y + scaledRect.size.height * 0.75)
-        
-        path.addCurveToPoint(dCurvePoint,
-                             controlPoint1: dControlPointOne,
-                             controlPoint2: dControlPointTwo)
-        
+        let path = UIBezierPath(roundedRect: scaledRect, cornerRadius: scaledRect.size.width * 0.05)
         
         
         path.closePath()
         return path
     }
 
+    func getSetaContainer(originalRect: CGRect, scale: Double) -> UIBezierPath{
+    
+        // Escala a largura do rect e corta seu tamanho escalado pela metade.
+        let scaledWidth = (originalRect.size.width * CGFloat(scale) * 0.5)
+        
+        //  Obtém a diferença da largura original para a escalada e divide por 2
+        let scaledXValue = ((originalRect.size.width) - scaledWidth) / 2
+        
+        // Escala a altura do rect
+        let scaledHeight = (originalRect.size.height * CGFloat(scale)  )
+        
+        //  Obtém a diferença da altura original para a escalada e divide por 2
+        let scaledYValue = ((originalRect.size.height) - scaledHeight * 1.5)
+        
+        
+        // Cria um novo rect
+        let scaledRect = CGRect(x: scaledXValue, y: scaledYValue, width: scaledWidth, height: scaledHeight)
+        
+        
+        
+        let path = UIBezierPath(rect: scaledRect)
+        
+        
+        
+        path.closePath()
+        return path
 
+        
+    
+    }
 
+    
+    func getSeta(rect: CGRect, scale: Double) -> UIBezierPath{
+    
+    
+        let path = UIBezierPath()
+        
+        // O caminho para o centro do componente
+        path.moveToPoint(CGPointMake(rect.size.width * 0.5, rect.size.height * 0.5))
+        
+        // Adiciona uma linha até o "pescoço" da seta
+        path.addLineToPoint(CGPointMake(rect.size.width * 0.5, rect.size.height * 0.10))
+        
+        // Move o caminho para o "pescoço"
+        path.moveToPoint(CGPointMake(rect.size.width * 0.5, rect.size.height * 0.10))
 
-
+        // Adiciona  x na direita da "cabeça" da seta
+        path.addLineToPoint(CGPointMake(rect.size.width * 0.55, rect.size.height * 0.10))
+        
+        // Adiciona  y na "cabeça" da seta
+        path.addLineToPoint(CGPointMake(rect.size.width * 0.5, rect.size.height * 0.03))
+    
+        // Adiciona  x esquerdo da "cabeça" da seta
+        path.addLineToPoint(CGPointMake(rect.size.width * 0.45, rect.size.height * 0.10))
+        path.closePath()
+        return path
+    
+    
+    }
 
 
 
